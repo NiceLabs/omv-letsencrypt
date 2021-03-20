12cc42ae5c40719ff7f3c463b55b5ac57a7aeb0b
Septs <github@septs.pw>
2021-03-20 11:30:20 +0800

    reduce deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# shellcheck disable=SC1091
 set -euo pipefail
 set -x
-python3 -m venv .venv
-.venv/bin/pip install -U pip
-.venv/bin/pip install certbot certbot-plugin-gandi
+if [ ! -d '.venv' ]; then
+	python3 -m venv .venv
+fi
+source .venv/bin/activate
+pip install -U pip
+pip install -U certbot certbot-plugin-gandi
